7c87b39444e86f25ea8d265354c6d03b05ccba84
iclm <iclm.dvl@gmail.com>
2018-10-13 18:52:35 +0700

    อัพไฟล์ครั้งแรก

---
+++ view/sidebar.php
@@ -0,0 +1,52 @@
+<div class="sidebar" data-color="purple" data-image="assets/img/sidebar-5.jpg">
+
+<!--
+
+    Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+    Tip 2: you can also add an image using data-image tag
+
+-->
+
+  <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="http://www.creative-tim.com" class="simple-text">
+                Creative Tim
+            </a>
+        </div>
+
+        <ul class="nav">
+            <li class="active">
+                <a href="home.php">
+                    <i class="pe-7s-graph"></i>
+                    <p>หน้าหลัก</p>
+                </a>
+            </li>
+            <li>
+                <a href="member.php">
+                    <i class="pe-7s-user"></i>
+                    <p>ข้อมูลสมาชิก</p>
+                </a>
+            </li>
+            <li>
+                <a href="test.php">
+                    <i class="pe-7s-note2"></i>
+                    <p>แบบทดสอบ</p>
+                </a>
+            </li>
+            <li>
+                <a href="score.php">
+                    <i class="pe-7s-news-paper"></i>
+                    <p>คะแนน</p>
+                </a>
+            </li>
+            <li>
+                <a href="media.php">
+                    <i class="pe-7s-science"></i>
+                    <p>วีดีโอ</p>
+                </a>
+            </li>
+
+
+
+  </div>
+</div>
